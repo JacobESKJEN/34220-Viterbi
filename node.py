e86c56965dc54310ed59f4087c6d621fc7211443
FrederikOeLar <184511953+FrederikOeLar@users.noreply.github.com>
2025-06-20 11:33:34 +0200

    Updated nodes

--- node.py
+++ node.py
@@ -1,9 +1,11 @@
+import numpy as np
+
 class Node:
     def __init__(self, minError):
         self.in0 = 0
         self.in1 = 0
-        self.in0Val = []
-        self.in1Val = []
+        self.out0 = np.array([], dtype=int)
+        self.out1 = np.array([], dtype=int)
         self.minError = minError
         self.decOut = 0
         self.cameFrom = 0
